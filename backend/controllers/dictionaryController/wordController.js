e024885a6135dd245e833f6589e2f9264566082b
shcohen770 <shcohen5780@gmail.com>
2023-12-06 17:54:46 +0200

    added a "word not found" alert

--- backend/controllers/dictionaryController/wordController.js
+++ backend/controllers/dictionaryController/wordController.js
@@ -1,32 +1,38 @@
-import { getfindByLetterDB, getfindWordDB } from "../../services/dictionary/wordServices.js";
+import {
+  getfindByLetterDB,
+  getfindWordDB,
+} from "../../services/dictionary/wordServices.js";
 
 const getfindWordController = async (req, res) => {
   try {
     const data = req.query;
-    if (data.original !== '') {
+    if (data.original !== "") {
       const result = await getfindWordDB(data);
       if (result) {
         return res.status(200).json({ data: result });
       } else {
-        return res.status(400).json({ error: 'Word not found' });
+        return res.status(200).json({ message: "Word not found", data: data });
       }
     } else {
-      return res.status(400).json({ error: 'You must enter a value in the search' });
+      return res
+        .status(400)
+        .json({ error: "You must enter a value in the search" });
     }
   } catch (error) {
     res.status(400).json({ error: error.message }); // Send an error response
   }
 };
 
-
 const getfindByLetterController = async (req, res) => {
   try {
     const data = req.query;
-    if (data.original !== '') {
+    if (data.original !== "") {
       const result = await getfindByLetterDB(data);
       return res.status(200).json({ data: result });
     } else {
-      return res.status(400).json({ error: 'You must enter a letter in the search' });
+      return res
+        .status(400)
+        .json({ error: "You must enter a letter in the search" });
     }
   } catch (error) {
     res.status(400).json({ error: error.message }); // Send an error response
--- fronted/src/Dashboard.jsx
+++ fronted/src/Dashboard.jsx
@@ -6,31 +6,40 @@ import ReactVirtualizedTable from "./components/VirtualizationTable";
 import Search from "./components/Search";
 import LetterSearch from "./components/LetterSearch";
 
-
 export default function Dashboard() {
-
   const [isSearchClicked, setIsSearchClicked] = useState(false);
   const [errors, setErrors] = useState(null);
   const [success, setSuccess] = useState(null);
   const [result, setResult] = useState(null);
   const [SearchByLetter, setSearchByLetter] = useState(null);
-
+  const [wordNotFound, setWorfNotFound] = useState(false);
+  const [searchedWord, setSearchedWord] = useState("");
 
   const handleSearchDataReceived = async (ReqByValue) => {
     try {
-
       let response;
 
       if (!SearchByLetter) {
-        response = await api.get(`/api/words/findWord?original=${ReqByValue.original}`);
+        response = await api.get(
+          `/api/words/findWord?original=${ReqByValue.original}`
+        );
       } else if (SearchByLetter) {
-        response = await api.get(`/api/words/findLetter?original=${ReqByValue.original}`);
+        response = await api.get(
+          `/api/words/findLetter?original=${ReqByValue.original}`
+        );
       }
 
       setSuccess(response.data.message);
+
       setResult(response.data.data);
-      setErrors("");
-      setIsSearchClicked(true); // Set isSearchClicked to true when data is received
+
+      if (response.data.message === "Word not found") {
+        setSearchedWord(response.data.data.original);
+        setWorfNotFound(true);
+      } else {
+        setErrors("");
+        setIsSearchClicked(true); // Set isSearchClicked to true when data is received
+      }
     } catch (error) {
       setResult("");
       setErrors(error.response.data.errors.join(", "));
@@ -39,9 +48,8 @@ export default function Dashboard() {
   };
 
   const handleSelectLetter = (event, reason) => {
-    setSearchByLetter(true)
-  }
-
+    setSearchByLetter(true);
+  };
 
   return (
     <div>
@@ -63,12 +71,31 @@ export default function Dashboard() {
           <LetterSearch
             onDataReceived={(data) => {
               handleSearchDataReceived(data);
-              handleSelectLetter()
+              handleSelectLetter();
             }}
           />
         </div>
 
         <div>{isSearchClicked && <ReactVirtualizedTable props={result} />}</div>
+        <div>
+          {wordNotFound && (
+            <div
+              style={{
+                margin: "auto",
+                width: "90%",
+                maxWidth: "500px",
+                overflowX: "auto",
+                marginTop: "15px",
+                backgroundColor: "#F6C927",
+              }}
+            >
+              <p>
+                the word <span style={{ color: "red" }}>{searchedWord}</span> is
+                not found
+              </p>
+            </div>
+          )}
+        </div>
       </div>
       <Footer />
     </div>
