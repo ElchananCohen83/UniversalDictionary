d7c823648666075a49ac3b306ebf6b8c1969ce8a
Elchanan Cohen <144008733+ElchananCohen83@users.noreply.github.com>
2023-12-13 12:34:31 +0200

    fix

--- fronted/src/Homepage.jsx
+++ fronted/src/Homepage.jsx
@@ -9,34 +9,25 @@ export default function HomePage() {
         display: "flex",
         flexDirection: "column",
         minHeight: "100vh", // Set minimum height to 100% of the viewport height
+        backgroundColor: "#21213E",
       }}
     >
+      <Header />
       <div
         style={{
-          flex: "1", // Allow the top section to grow and take available space
-          backgroundColor: "#21213E",
+          flex: "1", // Allow the content to grow and take available space
+          textAlign: "center",
+          color: "white",
+          padding: "20px", // Add padding for better readability
         }}
       >
-        <Header />
-        <div
-          style={{
-            textAlign: "center",
-            color: "white",
-            padding: "20px", // Add padding for better readability
-          }}
-        >
-          <h1 style={{ fontSize: '8vw', margin: '5vw' }}>המילון האוניברסלי</h1>
-          <h2 style={{ fontSize: '5vw' }}>עברי-אנגלי</h2>
-          <h2 style={{ fontSize: '5vw' }}>אנגלי-עברי</h2>
-          <h2>***</h2>
-          <h3 style={{ fontSize: '3vw' }}>עורכת ראשית: ינטי הופמן</h3>
-        </div>
+        <h1 style={{ fontSize: '8vw', margin: '5vw' }}>המילון האוניברסלי</h1>
+        <h2 style={{ fontSize: '5vw' }}>עברי-אנגלי</h2>
+        <h2 style={{ fontSize: '5vw' }}>אנגלי-עברי</h2>
+        <h2>***</h2>
+        <h3 style={{ fontSize: '3vw' }}>עורכת ראשית: ינטי הופמן</h3>
       </div>
-      <Footer
-        style={{
-          flexShrink: 0, // Prevent the footer from shrinking
-        }}
-      />
+      <Footer />
     </div>
   );
 }
