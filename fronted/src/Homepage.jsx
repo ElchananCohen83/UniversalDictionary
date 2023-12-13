6d3fda2903945ca60b003fd7b317bc69219d85cf
Elchanan Cohen <144008733+ElchananCohen83@users.noreply.github.com>
2023-12-13 12:41:47 +0200

    fix

--- fronted/src/Homepage.jsx
+++ fronted/src/Homepage.jsx
@@ -18,10 +18,10 @@ export default function HomePage() {
           flex: "1", // Allow the content to grow and take available space
           textAlign: "center",
           color: "white",
-          padding: "20px", // Add padding for better readability
+          // padding: "20px", // Add padding for better readability
         }}
       >
-        <h1 style={{ fontSize: '8vw', margin: '5vw' }}>המילון האוניברסלי</h1>
+        <h1 style={{ fontSize: '8vw', margin: '20vw' }}>המילון האוניברסלי</h1>
         <h2 style={{ fontSize: '5vw' }}>עברי-אנגלי</h2>
         <h2 style={{ fontSize: '5vw' }}>אנגלי-עברי</h2>
         <h2>***</h2>
