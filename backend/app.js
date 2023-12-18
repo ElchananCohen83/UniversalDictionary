a63d1af8db7bdba6ee89cee4bc1868851c566a9a
Elchanan Cohen <144008733+ElchananCohen83@users.noreply.github.com>
2023-12-18 17:32:33 +0200

    fix import.meta.url

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,30 @@
-import express from "express"
+import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
-import { connectToDatabase } from "./db/dbConnect.js"
+import { connectToDatabase } from "./db/dbConnect.js";
 import verifyToken from "./middleware/auth_JWT.js";
-import userRoutes from "./routes/userRoutes.js"
-import wordRoutes from "./routes/wordRoutes.js"
-
+import userRoutes from "./routes/userRoutes.js";
+import wordRoutes from "./routes/wordRoutes.js";
 import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
-app.use(verifyToken)
+app.use(verifyToken);
 
-app.use("/api/users", userRoutes)
-app.use("/api/words", wordRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/words", wordRoutes);
 
-app.use(express.static(path.join(__dirname, 'frontend')));
-
+const frontendPath = new URL('frontend', import.meta.url).pathname;
+app.use(express.static(path.join(frontendPath)));
 
 app.get('/nagishli_beta.js', (req, res) => {
   res.type('application/javascript');
-  res.sendFile(path.join(__dirname, 'frontend', 'nagishli_beta.js'));
+  res.sendFile(path.join(frontendPath, 'nagishli_beta.js'));
 });
 
-
-connectToDatabase()
+connectToDatabase();
 
 export default app;
